5e458b8f4da12fc3ca063714e01cc4f5a9e61801
scripthon <90333832+scripthon@users.noreply.github.com>
2023-09-29 14:56:44 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz
+tar -xvf tmate-2.4.0-static-linux-amd64.tar.xz
+./tmate-2.4.0-static-linux-amd64/tmate
